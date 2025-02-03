2fdbf1be736e42167d3355c0701812fb55a99016
Vinicius Milani <vmrfreitas@inf.ufrgs.br>
2025-02-03 17:31:31 -0300

    feat: add localdatabase settings and temporary password, modify user entity

--- src/entity/User.entity.ts
+++ src/entity/User.entity.ts
@@ -3,16 +3,10 @@ import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
 @Entity()
 export class User {
 
-    @PrimaryGeneratedColumn()
-    id: number
+    @PrimaryGeneratedColumn("uuid")
+    id: string
 
     @Column()
-    firstName: string
-
-    @Column()
-    lastName: string
-
-    @Column()
-    age: number
+    name: string
 
 }
